6d8ea206ee3f654a3c0b9c7ecc9faf90447d17e6
Ecstatik <johankool2003@hotmail.co.uk>
2023-07-19 11:50:09 +0100

    Codacy Fix

--- Toggl.Api/DataObjects/ReportTimeEntry.cs
+++ Toggl.Api/DataObjects/ReportTimeEntry.cs
@@ -28,28 +28,24 @@ public class ReportTimeEntry : BaseDataObject
 	public long? UserId { get; set; }
 
 	[JsonProperty(PropertyName = "user")]
-	public string? UserName { get; set; } = null;
+	public string? UserName { get; set; }
 
 	[JsonProperty(PropertyName = "description")]
 	public string? Description { get; set; }
 
 	[JsonProperty(PropertyName = "start")]
-	//[JsonConverter(typeof(IsoDateTimeConverter))]
-	//public DateTime? Start { get; set; }
 	public string? Start { get; set; }
 
 	[JsonProperty(PropertyName = "end")]
-	//[JsonConverter(typeof(IsoDateTimeConverter))]
-	//public DateTime? Stop { get; set; }
-	public string? Stop { get; set; } = null;
+
+	public string? Stop { get; set; }
 
 	[JsonProperty(PropertyName = "dur")]
 	public long? Duration { get; set; }
 
 	[JsonProperty(PropertyName = "updated")]
-	//[JsonConverter(typeof(IsoDateTimeConverter))]
-	//public DateTime? Stop { get; set; }
-	public string? Updated { get; set; } = null;
+
+	public string? Updated { get; set; }
 
 	[JsonProperty(PropertyName = "use_stop")]
 	public bool? UseStop { get; set; }
@@ -61,7 +57,7 @@ public class ReportTimeEntry : BaseDataObject
 	public long? Billable { get; set; }
 
 	[JsonProperty(PropertyName = "tags")]
-	public List<string>? TagNames { get; set; } = null;
+	public List<string>? TagNames { get; set; }
 
 	public override string ToString() => string.Format("Id: {0}, Start: {1}, Stop: {2}, TaskId: {3}", Id, Start, Stop, TaskId);
 }
