e97c23071dcc0583220b998fb1447c34d5db7a7d
Roland Banks <roland.banks@panoramicdata.com>
2024-05-10 13:50:29 +0700

    Fixed ClientStatus enum by adding [EnumMember] attributes.

--- Toggl.Api/Models/ClientStatus.cs
+++ Toggl.Api/Models/ClientStatus.cs
@@ -1,8 +1,15 @@
-﻿namespace Toggl.Api.Models;
+﻿using System.Runtime.Serialization;
+
+namespace Toggl.Api.Models;
 
 public enum ClientStatus
 {
+	[EnumMember(Value = "active")]
 	Active,
+
+	[EnumMember(Value = "archived")]
 	Archived,
+
+	[EnumMember(Value = "both")]
 	Both
 }
